5be0969b57b738ae1376151adb78d06004216bdf
Vineet Patel <vineetpatel725@gmail.com>
2024-06-04 08:59:49 +0530

    Added command to upgrade all changes to the database

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 # Command to export PYTHONPATH
 export PYTHONPATH=.
 
+# migrating all changes to the database
+alembic upgrade head
+
 # Command to start uvicorn server
 nohup python3 app/main.py -p >> ./backend.out &
 
